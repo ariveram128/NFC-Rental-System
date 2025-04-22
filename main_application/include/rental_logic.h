cb42761c90300acf6a5bf135eee860b46fa6c78e
Marvin Rivera <mrvnlxndrrvr2@gmail.com>
2025-04-22 16:03:46 -0400

    Implementation should now correctly handle standard NFC tags - FOUNDATION TO FP

--- main_application/include/rental_logic.h
+++ main_application/include/rental_logic.h
@@ -1,14 +1,26 @@
 #ifndef RENTAL_LOGIC_H
 #define RENTAL_LOGIC_H
 
-typedef struct {
-    char tag_id[32];
-    uint32_t start_time;
-    uint32_t duration;
-    bool active;
-} rental_data_t;
+// typedef struct {
+//     char tag_id[32];
+//     uint32_t start_time;
+//     uint32_t duration;
+//     bool active;
+// } rental_data_t;
 
-int process_rental(const char *tag_id);
-int check_rental_expiration(void);
+// int process_rental(const char *tag_id);
+// int check_rental_expiration(void);
+
+
+#include <stddef.h> // For size_t
+
+// Function called when an NFC tag scan provides an item ID
+void rental_logic_process_scan(const uint8_t *item_id, size_t item_id_len);
+
+// Maybe other functions later (e.g., rental_start, rental_end)
 
 #endif /* RENTAL_LOGIC_H */
+
+
+
+
---
+++ main_application/src/ble_handler.c
@@ -0,0 +1,7 @@
+#include "ble_handler.h"
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(ble_handler, LOG_LEVEL_INF);
+
+// This file will be implemented in Phase 2/3
